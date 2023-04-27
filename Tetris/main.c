26c5bb6359d75e76af3c7bcfa744bb185c44e724
fanhua <fanhua1216@gmail.com>
2023-04-27 10:05:18 +0800

    0427

--- Tetris/main.c
+++ Tetris/main.c
@@ -1,6 +1,28 @@
 #inclde<stdio.h>
 
 
+
+typedef enum {
+	RED	= 41,
+	YELLOW ,
+	GREEN ,
+	BLUE ,
+	PURPLE ,
+	CYAN ,
+	WHITE ,
+	BLACK = 0 
+}Color;
+
+typedef enum {
+	EMPTY = -1,
+	I,J,L,O,S,T,Z
+}ShapeID;
+
+typedef struct {
+	Color color;
+	ShapeID shape;
+}Block;
+
 int main() {
 
 }
