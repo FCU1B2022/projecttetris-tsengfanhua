149ff7a944e436a4277979f7a246c32dc31435d1
繁樺 曾 <fanhua1216@gmail.com>
2023-05-18 11:13:27 +0800

    0518

--- Tetris/main.c
+++ Tetris/main.c
@@ -2,6 +2,7 @@
 #include<stdbool.h>
 #include<stdlib.h>
 #include<time.h>
+#include<windows.h>
 
 #define CANVAS_WIDTH 10
 #define CANVAS_HEIGHT 20
@@ -248,6 +249,66 @@ void resetBlock(Block* block) {
 	block->current = false;
 }
 
+bool move(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int originalX, int originalY, int originalRotate, int newX, int newY, int newRotate, Shapeid shapeid) {
+	Shape shapeData = shapes[shapeid];
+	int size = shapeData.size;
+	int i, j;
+
+	// check if the new position is valid to place the block
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (shapeData.rotates[newRotate][i][j]) {
+				if (newX + j < 0 || newX + j >= CANVAS_WIDTH || newY + i < 0 || newY + i >= CANVAS_HEIGHT) {
+					return false;
+				}
+				if (!canvas[newY + i][newX + j].current && canvas[newY + i][newX + j].shape != EMPTY) {
+					return false;
+				}
+			}
+		}
+	}
+
+	// remove the old position
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (shapeData.rotates[originalRotate][i][j]) {
+				resetBlock(&canvas[originalY + i][originalX + j]);
+			}
+		}
+	}
+
+	// move the block
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (shapeData.rotates[newRotate][i][j]) {
+				setBlock(&canvas[newY + i][newX + j], shapeData.color, shapeId, true);
+			}
+		}
+	}
+
+	return true;
+}
+
+void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
+	printf("\033[0;0H\n");
+	int i,j;
+	for (i = 0; i < CANVAS_HEIGHT; i++) {
+		printf("|");
+		for (j = 0; j < CANVAS_WIDTH; j++) {
+			printf("\033[%dm\u3000", canvas[i][j].color);
+		}
+		printf("\033[0m|\n");
+	}
+	return;
+}
+
+void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
+	if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0])) {
+		state->y++;
+	}
+	return;
+}
+
 int main() {
 	srand(time(NULL));
 	State state = {
@@ -290,5 +351,11 @@ int main() {
 	}*/
 
 	system("cls");
-	printf("\e[?25l"); // hide cursor
+//	printf("\e[?25l"); // hide cursor
+
+	while (1) {
+		logic();
+		printCanvas(canvas);
+		Sleep(100);
+	}
 }
